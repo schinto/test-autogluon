ad72af31ee9b1b16fea441609f8161b539dfa58d
schinto <Torsten.Schindler@me.com>
2021-07-01 12:23:12 +0200

    Sets column with pre-defined time-splits used for evaluation on datasets with different features.

--- benchmark-autogluon-pih-model.py
+++ benchmark-autogluon-pih-model.py
@@ -7,15 +7,60 @@ from autogluon.tabular import TabularDataset, TabularPredictor
 from sklearn.model_selection import StratifiedKFold
 
 """
-Evaluate Autogluon performance using 5-fold random splits
-on the classification of molecular phototoxicity
-for different descriptors of molecules.
+Benchmark model based on different input data containing
+combinations of fingerprints and descriptors.
 
-Random splits may overestimate the performance of the models.
-It is better to use time-based, scaffold, or SCINS splits for the
-assessment of the model performance.
+Time-split evaluation is done by the values given in the Set column
+as described in the publication:
+https://doi.org/10.1021/acs.chemrestox.9b00338
 
 """
+
+
+def evaluate_autogluon_by_set(dataset_name, data_df, label_column, id_columns):
+    # Separate train and test data by pre-definded set
+    df = data_df.copy()
+    train_df = df[df["Set"] == "Train"].copy()
+    test_df = df[df["Set"] == "Test"].copy()
+
+    train_df.drop(columns=["Set"], inplace=True)
+    test_df.drop(columns=["Set"], inplace=True)
+    ignored_columns = id_columns.copy()
+    ignored_columns.remove("Set")
+
+    # Train model
+    predictor = TabularPredictor(
+        label=label_column,
+        path=f"./models/ag-binary-{dataset_name}-by-set",
+        problem_type="binary",
+        eval_metric="roc_auc",
+        learner_kwargs={"positive_class": "yes", "ignored_columns": ignored_columns},
+    )
+    predictor.fit(
+        # time_limit=10,
+        train_data=train_df,
+        presets="best_quality",
+        verbosity=0,
+    )
+
+    # Evaluate model on test data
+    y_pred = predictor.predict_proba(test_df.drop(columns=[label_column]))
+    y_true = test_df[label_column]
+    perf = predictor.evaluate_predictions(
+        y_true=y_true,
+        y_pred=y_pred,
+        auxiliary_metrics=True,
+        silent=True,
+        # detailed_report=True,
+    )
+    # Add fold and name to performance dictionary
+    perf["dataset"] = dataset_name
+
+    # Show roc auc of each fold and dataset
+    score = perf.get("roc_auc")
+    print(f"ROC-AUC: {score:.4f}\tdataset: {dataset_name}")
+
+    return perf
 
 
 def evaluate_autogluon(
@@ -90,10 +135,12 @@ def main():
 
         print("Starting:", name, data_df.shape)
         start = time.time()
-        nested_scores = evaluate_autogluon(
-            name, data_df, label_column, id_columns, n_splits=5
-        )
-        results.extend(nested_scores)
+        # scores = evaluate_autogluon(
+        #     name, data_df, label_column, id_columns, n_splits=5
+        # )
+        # results.extend(scores)
+        scores = evaluate_autogluon_by_set(name, data_df, label_column, id_columns)
+        results.append(scores)
         elapsed = time.time() - start
         times.append(elapsed)
         print("Done. Elapsed time:", elapsed)
@@ -102,17 +149,17 @@ def main():
     print(results)
     # Round results to 4 digits
     results = pd.DataFrame(results).round(4)
-    results.to_csv("results/results.csv", index=False)
+    results.to_csv("results/time_split_results.csv", index=False)
 
-    print("\nAggregated results:")
-    # Calculate mean and std of the model metrics evaluated on different test sets for each dataset
-    results_aggregated = (
-        results.drop(columns=["fold"]).groupby("dataset").agg(["mean", "std"]).round(4)
-    )
-    # Flatten MultiIndex columns from aggregation
-    results_aggregated.columns = results_aggregated.columns.map("|".join).str.strip("|")
-    print(results_aggregated)
-    results_aggregated.to_csv("results/results_aggregated.csv")
+    # print("\nAggregated results:")
+    # # Calculate mean and std of the model metrics evaluated on different test sets for each dataset
+    # results_aggregated = (
+    #     results.drop(columns=["fold"]).groupby("dataset").agg(["mean", "std"]).round(4)
+    # )
+    # # Flatten MultiIndex columns from aggregation
+    # results_aggregated.columns = results_aggregated.columns.map("|".join).str.strip("|")
+    # print(results_aggregated)
+    # results_aggregated.to_csv("results/results_aggregated.csv")
 
     print("\nModel times:")
     print(times)
